a2e9f0f8a9ad4b11170ad0ff04d6554ff05260e7
Anssi Kulju <anssi.kulju@gmail.com>
2020-04-07 20:53:52 +0300

    dwm-movestack patch
    - applied a patch to move windows in the stack

--- config.def.h
+++ config.def.h
@@ -77,6 +77,7 @@ static const char *termcmd[]  = { terminal, NULL };
 static const char *browsercmd[]  = { "chromium", NULL };
 static const char *vifmcmd[]  = { terminal,  "-e",  "/usr/local/bin/vifmrun", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -90,6 +91,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
